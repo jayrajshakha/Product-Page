93f346ff2d6f019e7a838e52656f991647da0d74
jay <jayrajshakha01@gmail.com>
2023-07-28 11:30:53 +0530

    Complite

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 
-console.log('---------------------------------)');
+const buttons = document.querySelectorAll(".rb");
+const productImg = document.querySelector("#image");
+const images =[ 
+    "images/image1.png",
+    "images/image2.png",
+    "images/image3.png"
+];
+
+let a = 0;
+      
+
+     for(button of buttons){
+
+        button.onclick = (e)=>{
+
+          productImg.src = images[a];
+           a++;
+
+           if(a > 2){
+              a = 0;
+           }
+        }
+       }
